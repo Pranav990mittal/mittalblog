c7ead8f0640bc434d63b2f63ec5faf0173d6e8b7
Pranav990mittal <pranavmittal990@gmail.com>
2025-08-06 21:13:35 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -64,10 +64,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider
+       <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem={false}
           disableTransitionOnChange
         >
           <Navbar />
